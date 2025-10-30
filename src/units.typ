Mc-Zen/zero

commit 8b76ef26a2608c582626511a765ffe73ef5a7110

[fix] spacing with angle units
Fixes #39

src/units.typ
@@ -250,6 +250,9 @@
   if num-state.math {
     separator = $separator$
   }
+  if unit.numerator.first().at(0) in ("°", "′", "″", sym.degree, sym.prime, sym.prime.double) { 
+    separator = none
+  }
 
   let result = {
     num(value, state: num-state, force-parentheses-around-uncertainty: true) // force parens around numbers with uncertainty

tests/zi/basic/test.typ
@@ -8,8 +8,10 @@
 // $12 #sym.space.thin "m" #sym.space.thin "s"^(-1)$ \
 #zi.kg[12] \
 #zi.candela[23] \
+#zi.arcminute[23] \
 #zi.arcsecond[23] \
 #zi.kN[23] \
+#zi.degree[45] \
 
 #zi.kN() // later make them types maybe? Then we can just call #zi.kN, that looks much better
 

commit 3d1ae80721a9a65f0ac29266aa39298bfbed48da

[fix] regression with units with empty numerator
Issue introduced in 8b76ef26a2608c582626511a765ffe73ef5a7110

src/units.typ
@@ -252,7 +252,9 @@
   if num-state.math {
     separator = $separator$
   }
-  if unit.numerator.first().at(0) in ("°", "′", "″", sym.degree, sym.prime, sym.prime.double) { 
+  
+  let angles = ("°", "′", "″", sym.degree, sym.prime, sym.prime.double)
+  if unit.numerator.len() > 0 and unit.numerator.at(0).at(0) in angles { 
     separator = none
   }
 